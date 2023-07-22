d8ec63fc9a648b6af76194cf8073ac79850c6398
jdiofhaso <66395711+jdiofhaso@users.noreply.github.com>
2023-07-22 08:18:21 -0400

    Create fart.js
    j

---
+++ fart.js
@@ -0,0 +1,34 @@
+let fard = Math.random() * 7
+switch(fard) {
+case(0) :
+    console.log('the fog is getting closer');
+    break;
+    
+    case(1) :
+    console.log('fart moment');
+    break;
+     
+    case(2) :
+    console.log('ja rupee more like zatub3ee');
+    break;
+
+    case(3) :
+    console.log('bingus is good yes');
+    break;
+    
+    case(4) :
+    console.log('sub 2 faard');
+    break;
+    
+    case(5) :
+    console.log('ssub 2 yor mom');
+    break;
+    
+    case(6) :
+    console.log('unsub from jarupe/zahtube/harry19');
+    break;
+    
+    case(7) :
+    console.log('whar momr');
+    break;
+};
