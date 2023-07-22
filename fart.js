5b56a9d63dc7a93884a32a8c415801476444fd92
jdiofhaso <66395711+jdiofhaso@users.noreply.github.com>
2023-07-22 08:22:01 -0400

    formatted it cause it still wasnt working
    i hate semicolons

--- fart.js
+++ fart.js
@@ -1,34 +1,34 @@
-var fard = Math.random() * 7
-switch(fard) {
-case(0) :
-    console.log('the fog is getting closer');
-    break;
-    
-    case(1) :
-    console.log('fart moment');
-    break;
-     
-    case(2) :
-    console.log('ja rupee more like zatub3ee');
+var fard = Math.random() * 7;
+switch (fard) {
+  case 0:
+    console.log("the fog is getting closer");
     break;
 
-    case(3) :
-    console.log('bingus is good yes');
+  case 1:
+    console.log("fart moment");
     break;
-    
-    case(4) :
-    console.log('sub 2 faard');
+
+  case 2:
+    console.log("ja rupee more like zatub3ee");
     break;
-    
-    case(5) :
-    console.log('ssub 2 yor mom');
+
+  case 3:
+    console.log("bingus is good yes");
     break;
-    
-    case(6) :
-    console.log('unsub from jarupe/zahtube/harry19');
+
+  case 4:
+    console.log("sub 2 faard");
     break;
-    
-    case(7) :
-    console.log('whar momr');
+
+  case 5:
+    console.log("ssub 2 yor mom");
     break;
-};
+
+  case 6:
+    console.log("unsub from jarupe/zahtube/harry19");
+    break;
+
+  case 7:
+    console.log("whar momr");
+    break;
+}
